1a0b40711da6e262855c206feaee4b289741f4a0
Asbury Cao <asbury.cao@gmail.com>
2022-10-13 16:32:50 -0700

    added routes to redirect to questions and completion pages

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -12,19 +12,63 @@ responses = []
 
 @app.get("/")
 def survey_start():
+    '''
+    displays home page of starting survey for user
+    '''
+
     title = survey.title
     instructions = survey.instructions
-    return render_template("survey_start.html",
-                            title = title,
-                            instructions = instructions)
+    return render_template(
+        "survey_start.html",
+        title = title,
+        instructions = instructions
+        )
 
-@app.get("/questions/<int:question_id>")
+@app.post("/begin")
+def redirect_questions():
+    """
+    redirects to first question
+    """
+
+    return redirect('/questions/0')
+
+
+@app.get('/questions/<int:question_id>')
 def handle_questions(question_id):
+    """
+    displays question, choice buttons, and Continue button
+    """
+
     print("question_id is a ------------>>>>>>>>>>>>>>>>>>>", type(question_id))
-    question = survey.questions[question_id].question
-    choice = survey.questions[question_id].choices
+    question = survey.questions[question_id]
     print("question: ------------>>>>>>>>>>>>>>>>>>>", question)
-    print("choice: ------------>>>>>>>>>>>>>>>>>>>", choice)
     return render_template("question.html",
-                            question = question,
-                            choice = choice)
+                            question = question)
+
+@app.post('/answer')
+def redirect_answers():
+    """
+    redirect to next question until last question and then moves to complete pg.
+    """
+
+    answer = request.form['answer']
+    responses.append(answer)
+    if len(responses) < len(survey.questions):
+        return redirect(f'/questions/{len(responses)}')
+    else:
+        return redirect('/complete')
+
+
+@app.get('/complete')
+def complete():
+    """
+    displays completion page
+    """
+
+    questions = survey.questions
+    len_questions = len(questions)
+
+    return render_template('completion.html',
+                            responses = responses,
+                            questions = questions,
+                            len_questions = len_questions)

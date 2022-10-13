1da45a3e5da691e03af9529df50b5d0ccebb627e
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-13 12:30:21 -0700

    Root directory

--- app.py
+++ app.py
@@ -4,6 +4,27 @@ from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get("/")
+def survey_start():
+    title = survey.title
+    instructions = survey.instructions
+    return render_template("survey_start.html",
+                            title = title,
+                            instructions = instructions)
+
+@app.get("/questions/<int:question_id>")
+def handle_questions(question_id):
+    print("question_id is a ------------>>>>>>>>>>>>>>>>>>>", type(question_id))
+    question = survey.questions[question_id].question
+    choice = survey.questions[question_id].choices
+    print("question: ------------>>>>>>>>>>>>>>>>>>>", question)
+    print("choice: ------------>>>>>>>>>>>>>>>>>>>", choice)
+    return render_template("question.html",
+                            question = question,
+                            choice = choice)
